98b23299c608152420ef943efecda7a9057e4b36
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-05 00:28:34 +0300

    add func EncryptAESGCM

--- internal/crypto/e2ee.go
+++ internal/crypto/e2ee.go
@@ -8,3 +8,30 @@ import (
     "errors"
     "io"
 )
+
+
+
+func EncryptAESGCM(plaintext, keyB64 string) (string, error) {
+    key, err := base64.StdEncoding.DecodeString(keyB64)
+    if err != nil {
+        return "", err
+    }
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return "", err
+    }
+
+    aesgcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return "", err
+    }
+
+    nonce := make([]byte, aesgcm.NonceSize())
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        return "", err
+    }
+
+    ciphertext := aesgcm.Seal(nonce, nonce, []byte(plaintext), nil)
+    return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
