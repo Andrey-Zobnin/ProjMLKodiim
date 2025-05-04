7c873123a9b5e5fd069a9ada92e6ad036e76289b
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-04 16:28:46 +0300

    add all ml code

--- socnetwork/ml.go
+++ socnetwork/ml.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"bytes"
 	"context"
-	"io"
 	"net/http"
 	"encoding/json"
+	"io"
 )
 
 type MlService struct {
@@ -15,4 +15,39 @@ type MlService struct {
 
 func Mlservice(endpoint string) *Mlservice {
 	return &MlService{PythonEndpoint: endpoint}
+}
+
+type SearchRequest struct {
+	TopK  int    `json:"top_k"`
+	Query string `json:"query"`
+}
+
+type SearchResult struct {
+	Results []string `json:"results"`
+}
+
+func (ml *MLService) SemanticSearch(ctx context.Context, query string, topK int) ([]string, error) {
+	reqBody := SearchRequest{
+		Query: query,
+		TopK:  topK,
+	}
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(ml.PythonEndpoint+"/search", "application/json", bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		data, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("Python API error: %s", data)
+	}
+
+	var result SearchResult
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	return result.Results, err
 }
